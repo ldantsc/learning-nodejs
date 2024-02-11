2f6210f31d0bb835d9004a578ca4ce9ea8d2a0ce
Lucas Dantas <dants.dev@gmail.com>
2024-02-11 15:01:14 -0300

    configurando body parser

--- project-node/server.js
+++ project-node/server.js
@@ -1,17 +1,29 @@
-const handlebars = require('express-handlebars')
-const express = require('express')
-const app = express()
-const PORT = 3000
-
+const path = require("path");
+const { engine } = require("express-handlebars");
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
 // config handlebars
-app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+app.set("views", path.join(__dirname, "/views"));
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// rotas
+app.get("/cadastro", (req, res) => {
+  res.render("formulario");
+});
+
+app.post("/add", (req, res) => {
+  res.render("req.body.username" + req.body.username + req.body.email);
+});
 
 app.listen(PORT, () => {
-    console.log("conectado")
-})
+  console.log("conectado");
+});
